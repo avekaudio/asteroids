1ade3a1fa6d48823a7c8c726344871b97467aef4
avekaudio <danesmithhh@gmail.com>
2025-03-04 22:26:34 -0500

    asteroid and asteroid field classes added

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-dt = 0
 
                 
 def main():
-    global dt
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
         screen.fill("black")
-        for player in drawable:
-            player.draw(screen) 
-        updatable.update(dt) 
+
+        for obj in drawable:
+            obj.draw(screen) 
+     
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick(60) / 1000
+        ticker = clock.tick(60)
+        dt = ticker / 1000
                        
 
 if __name__ == "__main__":
